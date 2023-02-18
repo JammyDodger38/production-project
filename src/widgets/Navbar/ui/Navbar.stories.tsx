5f25b3c07c011a456f5e39a83d5886f25ebe735d
Vlad Vladimirov <blad20002000@mail.ru>
2023-02-19 02:07:13 +0700

    minor fix

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
-import { Navbar } from './Navbar';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import { Navbar } from './Navbar'
 
 export default {
-    title: 'widget/Navbar',
-    component: Navbar,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
-} as ComponentMeta<typeof Navbar>;
+  title: 'widget/Navbar',
+  component: Navbar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Navbar>
 
-const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: ComponentStory<typeof Navbar> = args => <Navbar {...args} />
 
-export const Light = Template.bind({});
-Light.args = {};
+export const Light = Template.bind({})
+Light.args = {}
 
-export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+export const Dark = Template.bind({})
+Dark.args = {}
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
