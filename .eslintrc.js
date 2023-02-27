19c2c7957f6310b625d6eb148976ea2b87b8d96a
Vlad Vladimirov <blad20002000@mail.ru>
2023-02-27 22:33:20 +0700

    fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
+    'arrow-parens': 'off',
     semi: 'off',
     'i18next/no-literal-string': [
       'error',
