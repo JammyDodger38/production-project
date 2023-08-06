c928d8fbae7a6d163b97e647bff78bb80dcfddbe
Vlad Vladimirov <blad20002000@mail.ru>
2023-08-06 13:14:12 +0700

    add skeleton, code, article details card

--- .fttemplates/TS Component/[FTName].tsx
+++ .fttemplates/TS Component/[FTName].tsx
@@ -1,19 +1,19 @@
-import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './[FTName].module.scss';
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTranslation } from 'react-i18next'
+import { memo } from 'react'
+import cls from './[FTName].module.scss'
 
 interface [FTName]Props {
-   className?: string;
+  className?: string
 }
 
-export const [FTName]: FC<[FTName]Props> = (props) => {
-   const { className } = props;
-   const { t } = useTranslation()
+export const [FTName] = memo((props: [FTName]Props) => {
+  const { className } = props
+  const { t } = useTranslation()
 
-   return (
-      <div className={classNames(cls.[FTName | camelcase], {}, [className])}>
-
-      </div>
-   );
-}
+  return (
+    <div className={classNames(cls.[FTName], {}, [className])}>
+      {t('[FTName]')}
+    </div>
+  )
+})
